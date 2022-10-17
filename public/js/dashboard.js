a8bb29933bf4c0615baed3c2ee51afc6b93e8e25
Nesha Whit <neshawhitmire@gmail.com>
2022-10-16 22:28:14 -0400

    troublshooting date and fixed userposts

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,10 +4,10 @@ const userBlogHandler = async (event) => {
     const title = document.querySelector('#title').value.trim();
     const userpost_content = document.querySelector('#content').value.trim();
   
-    if (title && userpost_content && date_created) {
+    if (title && userpost_content) {
       const response = await fetch('/api/blogs', {
         method: 'POST',
-        body: JSON.stringify({title, userpost_content, date_created}),
+        body: JSON.stringify({title, userpost_content}),
         headers: { 'Content-Type': 'application/json' },
       });
   
